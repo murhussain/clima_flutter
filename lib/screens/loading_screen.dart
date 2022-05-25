ffb96c139c71bdc251d1f3326e44d70effaad7a1
murhussain <husseinmurashi@yahoo.com>
2022-05-25 15:48:48 +0200

    done location tracing

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,16 +1,49 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-
+import '../services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
+
 class _LoadingScreenState extends State<LoadingScreen> {
-  void getLocation() async {
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-    print(position);
+  double? latitude;
+  double? longitude;
+
+  @override
+  void initState() {
+    super.initState();
+
+    getLocation();
+    getData();
   }
+
+  void getLocation() async {
+    Location location = Location();
+
+    await location.getCurrentLocation();
+
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    print('the latitude is: ${location.latitude}');
+    print('the longitude ff is: ${location.longitude}');
+    print('the longitude is: $longitude');
+  }
+
+  void getData() async {
+    http.Response response = await http.get(Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=1e9f450ee349d837e72df15654452322'));
+    
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else{
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
